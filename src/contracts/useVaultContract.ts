a7710af4fbd0128a232a75729439ddd6fd0adaf5
Lakindu Ravishka <237532771+Lakindu-R@users.noreply.github.com>
2026-02-23 22:41:24 +0530

    feat(useVaultContex): change vault address address

--- src/contracts/useVaultContract.ts
+++ src/contracts/useVaultContract.ts
@@ -2,7 +2,7 @@ import { ethers } from "ethers";
 import { useWallet } from "../context/WalletContext";
 import VaultABI from "./TokenVault.json";
 
-const VAULT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const VAULT_ADDRESS = "0x3698388f631d4Fb2FE87C63d3D0c7e36b023d23A";
 
 export function useVaultContract(write = false) {
   const { provider, signer } = useWallet();
